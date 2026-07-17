ebe8b16f78563bacaf76c084af00bc10a0060e82
DiceRoll <diceroll.vrc@gmail.com>
2026-07-17 21:30:15 +0900

    サイズ変更ボタンを押してGridとThumbのサイズを変更

--- VRCAvatarManagerCE/js/app.js
+++ VRCAvatarManagerCE/js/app.js
@@ -3,7 +3,16 @@ const data = [...Array(20)].map((_,i)=>({name:'Avatar '+(i+1)}));
 const grid = document.getElementById('grid');
 const modal = document.getElementById('modal');
 
-// Fuction
+const sizeSmall = document.getElementById('sizeSmall');
+const sizeMedium = document.getElementById('sizeMedium');
+const sizeLarge = document.getElementById('sizeLarge');
+const sizeButtons = [
+    sizeSmall,
+    sizeMedium,
+    sizeLarge
+];
+
+/* Fuction */
 
 function render(list)
 {
@@ -20,6 +29,51 @@ function render(list)
     });
 }
 
-// Display
+function changeCardSize(width, thumbHeight)
+{
+    document.documentElement.style.setProperty(
+        "--card-width",
+        width
+    );
 
+    document.documentElement.style.setProperty(
+        "--thumb-height",
+        thumbHeight
+    );
+}
+
+function setActive(button)
+{
+    sizeButtons.forEach(b => b.classList.remove("active"));
+    button.classList.add("active");
+}
+
+/* ChangeCardSize */
+
+sizeSmall.addEventListener('click', () => changeCardSize('140px', '90px')); // 120
+sizeMedium.addEventListener('click', () => changeCardSize('220px', '150px')); // 200
+sizeLarge.addEventListener('click', () => changeCardSize('340px', '240px')); // 320
+
+sizeSmall.onclick = () =>
+{
+    changeCardSize("140px", "90px");
+    setActive(sizeSmall);
+};
+sizeMedium.onclick = () =>
+{
+    changeCardSize("220px", "150px");
+    setActive(sizeMedium);
+};
+sizeLarge.onclick = () =>
+{
+    changeCardSize("340px", "240px");
+    setActive(sizeLarge);
+};
+
+/* Call Function */
+
+// Display Cards
 render(data);
+
+// Init 
+setActive(sizeMedium);
