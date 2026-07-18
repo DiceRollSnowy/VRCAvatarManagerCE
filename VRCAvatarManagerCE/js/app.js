fd66e9d64594866b97158bfac8c028a8e5ed9e84
DiceRoll <diceroll.vrc@gmail.com>
2026-07-18 13:01:37 +0900

    テストアバターデータをダイアログに表示する処理を追加

--- VRCAvatarManagerCE/js/app.js
+++ VRCAvatarManagerCE/js/app.js
@@ -1,7 +1,21 @@
+/* data */
+
 const data = [...Array(20)].map((_,i)=>({name:'Avatar '+(i+1)}));
 
+const avatar = {
+    id: "avtr_xxxxx",
+    name: "シフォン",
+    description: "シフォン012",
+    thumbnail: "aaa.jpg",
+    unity: "2022.3.22f1",
+    platform: "standalonewindows",
+    createdat: "2026-07-01 11:22:33",
+    updatedat: "2026-07-02 11:22:33",
+};
+
+/* const */
+
 const avatarGrid = document.getElementById('avatarGrid');
-const avatarModal = document.getElementById('avatarDetailModal');
 
 const sizeSmallBtn = document.getElementById('sizeSmallBtn');
 const sizeMediumBtn = document.getElementById('sizeMediumBtn');
@@ -11,6 +25,19 @@ const sizeButtons = [
     sizeMediumBtn,
     sizeLargeBtn
 ];
+
+/* const AvatarDetail */
+
+const avatarModal = document.getElementById('avatarDetailModal');
+
+const avatarId = document.getElementById("avatarId");
+const avatarName = document.getElementById("avatarName");
+const avatarDescription = document.getElementById("avatarDescription");
+const avatarThumbnail = document.getElementById("avatarThumbnail");
+const avatarUnityVer = document.getElementById("avatarUnityVer");
+const avatarPlatform = document.getElementById("avatarPlatform");
+const avatarCreatedAt = document.getElementById("avatarCreatedAt");
+const avatarUpdatedAt = document.getElementById("avatarUpdatedAt");
 
 /* Fuction */
 
@@ -28,8 +55,7 @@ function render(list)
 
         const detailBtn = card.querySelector('#detailBtn');
         detailBtn.onclick=()=>{
-            //t.textContent=a.name;
-            avatarModal.showModal();
+            showAvatarDetail(avatar);
         };
         const changeBtn = card.querySelector('#changeBtn');
         changeBtn.onclick=()=>{
@@ -57,6 +83,18 @@ function setActive(button)
 {
     sizeButtons.forEach(b => b.classList.remove("active"));
     button.classList.add("active");
+}
+
+function showAvatarDetail(avatar)
+{
+    avatarName.textContent = avatar.name;
+    avatarDescription.textContent = avatar.description;
+    //avatarThumbnail.src = avatar.thumbnail;
+    avatarUnityVer.textContent = avatar.unity;
+    avatarPlatform.textContent = avatar.platform;
+    avatarCreatedAt.textContent = avatar.createdat;
+    avatarUpdatedAt.textContent = avatar.updatedat;
+    avatarModal.showModal();
 }
 
 /* ChangeCardSize */
