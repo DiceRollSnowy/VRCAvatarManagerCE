a942e3780cf584fa9959f613ea572eb8fd934def
DiceRoll <diceroll.vrc@gmail.com>
2026-07-22 15:22:30 +0900

    サムネイル画像の読み込み失敗時にリトライ3回まで行うように変更

--- VRCAvatarManagerCE/js/services/ImageLoader.js
+++ VRCAvatarManagerCE/js/services/ImageLoader.js
@@ -3,7 +3,7 @@ export class ImageLoader
     static #queue = [];
     static #loading = false;
     static #cache = new Map();
-    
+
     static enqueue(img, url)
     {
         this.#queue.push({img, url});
@@ -31,15 +31,41 @@ export class ImageLoader
             }
 
             // 初回のみ取得 
-            item.img.onload = () => {
-                this.#cache.set(item.url, item.img.src); 
-            };
-            item.img.src = item.url;
+            await this.#loadImage(item);
             
-            await this.#sleep(200);
+            await this.#sleep(250);
         }
 
         this.#loading = false;
+    }
+
+    static async #loadImage(item, retry = 0)
+    {
+        return new Promise(resolve =>
+        {
+            item.img.onload = () =>
+            {
+                resolve(true);
+            };
+
+            item.img.onerror = async () =>
+            {
+                if (retry < 3)
+                {
+                    console.warn(`Load Image Failed. (Count:${retry + 1})`);
+                    await this.#sleep(250);
+                    resolve(
+                        this.#loadImage(item, retry + 1)
+                    );
+                }
+                else
+                {
+                    console.error("Load Image Failed.", item.url);
+                    resolve(false);
+                }
+            };
+            item.img.src = item.url;
+        });
     }
 
     static #sleep(ms)
